96c5d8c2ce52bb2afc5f71d2d3bd43d0912cf9ac
the-gingerbread-man <alex.patch3@gmail.com>
2016-04-05 14:00:05 -0700

    Re-located database files to /server/db

---
+++ server/db/articleModel.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import mongoose from 'mongoose';
+
+const articleSchema = mongoose.Schema({
+	id: {type: String, required: true},
+	title: {type: String, required: true},
+	updated: {type: Date, required: true},
+	contributors: {type: [String], required: true},
+	tags: {type: [String], required: true},
+	content: {type: [String], required: true}
+});
+
+module.exports = mongoose.model('Article', articleSchema);
