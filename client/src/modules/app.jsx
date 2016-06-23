7aea5a08c476469017d051dfbb2c93130750309b
Alex Patch <alex.patch3@gmail.com>
2016-06-22 22:49:34 -0700

    Updated things, added license headers. Cleaning up pre- major architectural update. (#3)
    * Updated README with Trello
    
    * Updated README with Trello
    
    * Updated things, added license headers

--- client/src/modules/app.jsx
+++ client/src/modules/app.jsx
@@ -1,3 +1,27 @@
+/*
+
+Copyright (c) 2016 [Alex Patch]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 import React from 'react';
 import { Link } from 'react-router';
 import fetch from 'whatwg-fetch';
@@ -10,24 +34,20 @@ class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {topArticles: []};
+		this.state = { topArticles: [] };
 	}
 
 	componentDidMount() {
-
 		fetch('/articles')
-			.then(response => {
-				return response.json();
-			}).then(json => {
-				this.setState(topArticles: json);
-			})
+			.then(response => response.json())
+			.then(json => this.setState(topArticles: json))
 			.catch(err => {
-				throw err;
+				throw err; // TODO: Improove error-handling
 			});
 	}
 
 	render() {
-		let articleHeadlines = this.state.topArticles.map((article) => {
+		let articleHeadlines = this.state.topArticles.map(article => {
 			return (
 				<Headline key={article.id} title={article.title} updated={moment(article.updated).format('MMMM Do YYYY, hh:mm a')} />
 				);
